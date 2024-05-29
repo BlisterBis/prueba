cc5620016769bfe5c7689cfb6a308c5fdce69a65
Blister <61089900+BlisterBis@users.noreply.github.com>
2024-05-29 08:43:31 +0200

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -1,15 +1,15 @@
 if ($response.status === 200) {
-  const body = $response.body; 
-  let obj = JSON.parse(body);
-  if ("feature_gates" in obj && typeof obj["feature_gates"] === 'object') {
-    for (const key in obj["feature_gates"]) {
-       if (obj["feature_gates"].hasOwnProperty(key)) {
-         obj["feature_gates"][key]["value"] = true;
-       }
+    let body = $response.body; 
+    let obj = JSON.parse(body);
+    if ("feature_gates" in obj) {
+        for (let key in obj["feature_gates"]) {
+            if (obj["feature_gates"].hasOwnProperty(key)) {
+                obj["feature_gates"][key]["value"] = true;
+            }
+        }
     }
-  }
-  const modifiedBody = JSON.stringify(obj);
-  $done(modifiedBody);
+    body = JSON.stringify(obj);
+    $done({ body });
 } else {
-  $done($response.body);
+    $done({});
 }
