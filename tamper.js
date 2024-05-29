8478bce93495d96fafd82357e21341d6632c8e8d
Blister <61089900+BlisterBis@users.noreply.github.com>
2024-05-29 08:38:35 +0200

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -1,12 +1,11 @@
-var body = $response.body;
-  if ($response.status === 200) {
-    var obj = JSON.parse(body);
-    if ("feature_gates" in obj) {
-      for (var key in obj["feature_gates"]) {
-        // Cambiar todas las 'feature gates' a true
-        obj["feature_gates"][key]["value"] = true;
-      }
+if ($response.status === 200) {
+  var body = $response.body; 
+  var obj = JSON.parse(body);
+  if ("feature_gates" in obj) {
+    for (var key in obj["feature_gates"]) {
+       obj["feature_gates"][key]["value"] = true;
     }
-    body = JSON.stringify(obj);
   }
-  $done(body);
+  body = JSON.stringify(obj);
+}
+$done(body);
